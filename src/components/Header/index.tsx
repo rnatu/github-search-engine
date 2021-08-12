de8cabaac8237a92373f94e160d6fffa71484da4
Renato Xavier <rnatu91@gmail.com>
2021-08-12 17:07:37 -0300

    feat(Home): add html on Home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,35 +5,12 @@ import closeIcon from "../../assets/images/close-icon.svg";
 import searchIcon from "../../assets/images/search-icon.svg";
 
 import { FormEvent } from "react";
-import { useLocation } from "react-router-dom";
-import axios from "axios";
 
-export function Header() {
+interface HeaderProps {
+  isHomePage: boolean;
+}
 
-  function useQuery() {
-    return new URLSearchParams(useLocation().search);
-  }
-
-  const query = useQuery();
-  const code = query.get("code");
-
-  if (!code) console.log("nada ainda");
-
-  console.log(code);
-
-  // if (code) {
-  //   axios
-  //     .post("https://github.com/login/oauth/access_token", null, {
-  //       params: {
-  //         client_id: "70019ed02828eaa939a8",
-  //         client_secret: "1897b6221e938325e2c32b36172005fc624abde2",
-  //         code: code,
-  //       },
-  //     })
-  //     .then((response) => console.log(response.status))
-  //     .catch((err) => console.log(err));
-  // }
-
+export function Header({ isHomePage = true }: HeaderProps) {
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
     console.log(event);
@@ -48,26 +25,28 @@ export function Header() {
         <img src={copyImg} alt="Github logo" />
       </div>
 
-      <div className="search-bar">
-        <form onSubmit={handleSubmit}>
-          <div className="icon-search">
-            <img src={searchIcon} alt="search icon" />
-            <input type="text" placeholder="username" />
-          </div>
-          <div className="actions-btn">
-            <button className="clear-btn">
-              <img src={closeIcon} alt="clear input" />
-            </button>
-            <button type="submit" className="submit-btn">
-              Search
-            </button>
-          </div>
-        </form>
-      </div>
-
-      <a href="https://github.com/login/oauth/authorize?client_id=70019ed02828eaa939a8">
-        <button>Login with github</button>
-      </a>
+      {isHomePage ? (
+        <div className="sign-in-btn">
+          <button>Sign in with github account</button>
+        </div>
+      ) : (
+        <div className="search-bar">
+          <form onSubmit={handleSubmit}>
+            <div className="icon-search">
+              <img src={searchIcon} alt="search icon" />
+              <input type="text" placeholder="username" />
+            </div>
+            <div className="actions-btn">
+              <button className="clear-btn">
+                <img src={closeIcon} alt="clear input" />
+              </button>
+              <button type="submit" className="submit-btn">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
     </header>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
 import { Header } from "../components/Header";
+import demoImage  from '../assets/images/demo-image.png';
+
 
 export function Home() {
   return (
     <>
-      <Header/>
+      <Header isHomePage />
+
+      <main>
+        <div className="title">
+          <h1>Github</h1>
+          <span>Where the world build software</span>
+        </div>
+
+        <div>
+          <div className="description">
+            <p>
+              Millions of developers and companies build, ship, and maintain
+              their software on GitHub—the largest and most advanced development
+              platform in the world.
+            </p>
+            <p>
+              It was commonly used to host open-source projects. As of January
+              2020, GitHub reports having over 40 million users and more than
+              190 million repositories (including at least 28 million public
+              repositories). It is the largest source code host as of April 2020
+            </p>
+          </div>
+          <div>
+            <img src={demoImage} alt="" />
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
