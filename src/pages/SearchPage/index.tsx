b7566d1442e1aaa5de8fa88898a553d7c1887b4b
Renato Xavier <rnatu91@gmail.com>
2021-08-13 16:09:07 -0300

    feat(SearchPage): add user cart with github data

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,31 +1,47 @@
-import { Header } from "../../components/Header";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useUser } from "../../hooks/useUser";
+import { useParams } from "react-router-dom";
 
-//!TO REMOVE
-import profileImage from "../../assets/profile21.png";
+import { SearchCard } from "../../components/SearchCard";
+import { Header } from "../../components/Header";
+
+import { api } from "../../services/api";
 import gitRepositoryIcon from "../../assets/images/git-repository-icon.svg";
 import gitStarIcon from "../../assets/images/git-star-icon.svg";
 
 import "./styles.scss";
-import { useParams } from "react-router-dom";
-import { SearchCard } from "../../components/SearchCard";
 
 type ParamType = {
   user: string;
 };
 
+type UserDataType = {
+  name: string;
+  location: string;
+  bio: string;
+  avatar_url: string;
+  html_url: string;
+};
+
 export function SearchPage() {
   const [searchOption, setSearchOptions] = useState("");
+  const [userData, setUserData] = useState({} as UserDataType);
   const { user } = useParams<ParamType>();
 
-  const { setUsername } = useUser();
-
   useEffect(() => {
-    setUsername(user);
-    setSearchOptions('');
-  }, [setUsername, user]);
+    (async function getUserData() {
+      const { data } = await api.get(`https://api.github.com/users/${user}`);
+      const filteredData = {
+        name: data.name,
+        location: data.location,
+        bio: data.bio,
+        avatar_url: data.avatar_url,
+        html_url: data.html_url,
+      };
+
+      setUserData(filteredData);
+    })();
+  }, [user]);
 
   return (
     <>
@@ -34,20 +50,17 @@ export function SearchPage() {
         <section className="user-dashboard">
           <div className="user-card">
             <div className="avatar">
-              <img src={profileImage} alt="Profile avatar" />
+              <img src={userData.avatar_url} alt="Profile avatar" />
             </div>
             <div className="user-description">
               <h1>
-                Renato Xavier <span>Brasil - SP</span>
+                {userData.name} <span>{userData.location}</span>
               </h1>
-              <p>
-                A JavaScript lover, motivated by solve problems and create
-                solutions! Currently learning and practicing ReactJS, NextJS and
-                TypeScript
-              </p>
+              <p>{userData.bio}</p>
 
               <p>
-                Visit the full Github profile by <a href="/">clicking here</a>
+                Visit the full Github profile by{" "}
+                <a href={userData.html_url}>clicking here</a>
               </p>
             </div>
           </div>
@@ -66,17 +79,17 @@ export function SearchPage() {
         {searchOption === "repositories" && (
           <div className="repository-container">
             <h1>21 Repositories</h1>
-            <SearchCard data={'sdad'}/>
-            <SearchCard data={'sdad'}/>
+            <SearchCard data={"sdad"} />
+            <SearchCard data={"sdad"} />
           </div>
         )}
 
         {searchOption === "starred" && (
-           <div className="repository-container">
-           <h1>22 Starreds</h1>
-           <SearchCard data={'sdad'}/>
-           <SearchCard data={'sdad'}/>
-         </div>
+          <div className="repository-container">
+            <h1>22 Starreds</h1>
+            <SearchCard data={"sdad"} />
+            <SearchCard data={"sdad"} />
+          </div>
         )}
       </main>
     </>
