e0ad1f12be6252848b59971660a527bbc5f7dc7b
Renato Xavier <rnatu91@gmail.com>
2021-08-14 14:47:07 -0300

    Feat(SearchPage): add loading flow

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import './styles.scss'
+
+export function Loading() {
+  return (
+    <div className="loading">
+      <div className="lds-dual-ring" />
+      <h2>Carregando...</h2>
+    </div>
+  );
+}
--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { api } from "../../services/api";
 
 import { SearchCard } from "../../components/SearchCard";
 import { Header } from "../../components/Header";
+import { Loading } from '../../components/Loading'
 
-import { api } from "../../services/api";
 import gitRepositoryIcon from "../../assets/images/git-repository-icon.svg";
 import gitStarIcon from "../../assets/images/git-star-icon.svg";
+import searchError from "../../assets/images/search-error.svg";
 
 import "./styles.scss";
 
@@ -27,72 +29,88 @@ export function SearchPage() {
   const [searchOption, setSearchOptions] = useState("");
   const [userData, setUserData] = useState({} as UserDataType);
   const { user } = useParams<ParamType>();
+  const [isDataFound, setIsDataFound] = useState<Boolean>();
 
   useEffect(() => {
     (async function getUserData() {
-      const { data } = await api.get(`https://api.github.com/users/${user}`);
-      const filteredData = {
-        name: data.name,
-        location: data.location,
-        bio: data.bio,
-        avatar_url: data.avatar_url,
-        html_url: data.html_url,
-      };
+      try {
+        const { data } = await api.get(`https://api.github.com/users/${user}`);
 
-      setUserData(filteredData);
-      setSearchOptions('')
+        setUserData(data);
+
+        setSearchOptions("");
+
+        setIsDataFound(true);
+      } catch (err) {
+        setIsDataFound(false);
+      }
     })();
   }, [user]);
 
   return (
     <>
       <Header />
-      <main className="user-search">
-        <section className="user-dashboard">
-          <div className="user-card">
-            <div className="avatar">
-              <img src={userData.avatar_url} alt="Profile avatar" />
+
+      {isDataFound === undefined && (
+        <Loading />
+      )}
+
+      {isDataFound === false && (
+        <div className="data-not-found">
+          <img src={searchError} alt="" />
+          <h2>Oops, something wrong happened</h2>
+          <h3>Please, Try search again</h3>
+        </div>
+      )}
+
+      {isDataFound && (
+        <main className="user-search">
+          <section className="user-dashboard">
+            <div className="user-card">
+              <div className="avatar">
+                <img src={userData.avatar_url} alt="Profile avatar" />
+              </div>
+              <div className="user-description">
+                <h1>
+                  {userData.name} <span>{userData.location}</span>
+                </h1>
+                <p>{userData.bio}</p>
+
+                <p>
+                  Visit the full Github profile by{" "}
+                  <a href={userData.html_url}>clicking here</a>
+                </p>
+              </div>
             </div>
-            <div className="user-description">
-              <h1>
-                {userData.name} <span>{userData.location}</span>
-              </h1>
-              <p>{userData.bio}</p>
-
-              <p>
-                Visit the full Github profile by{" "}
-                <a href={userData.html_url}>clicking here</a>
-              </p>
+            <div className="user-search-options">
+              <button onClick={() => setSearchOptions("repositories")}>
+                <img src={gitRepositoryIcon} alt="Repository icon" />
+                Search for Repositories
+              </button>
+              <button onClick={() => setSearchOptions("starred")}>
+                <img src={gitStarIcon} alt="Star icon" />
+                Search for Starred
+              </button>
             </div>
-          </div>
-          <div className="user-search-options">
-            <button onClick={() => setSearchOptions("repositories")}>
-              <img src={gitRepositoryIcon} alt="Repository icon" />
-              Search for Repositories
-            </button>
-            <button onClick={() => setSearchOptions("starred")}>
-              <img src={gitStarIcon} alt="Star icon" />
-              Search for Starred
-            </button>
-          </div>
-        </section>
+          </section>
 
-        {searchOption === "repositories" && (
-          <div className="repository-container">
-            <h1>21 Repositories</h1>
-            <SearchCard data={"sdad"} />
-            <SearchCard data={"sdad"} />
-          </div>
-        )}
+          {searchOption === "repositories" && (
+            <div className="repository-container">
+              <h1>21 Repositories</h1>
+              <SearchCard data={"sdad"} />
+              <SearchCard data={"sdad"} />
+            </div>
+          )}
 
-        {searchOption === "starred" && (
-          <div className="repository-container">
-            <h1>22 Starreds</h1>
-            <SearchCard data={"sdad"} />
-            <SearchCard data={"sdad"} />
-          </div>
-        )}
-      </main>
+          {searchOption === "starred" && (
+            <div className="repository-container">
+              <h1>22 Starreds</h1>
+              <SearchCard data={"sdad"} />
+              <SearchCard data={"sdad"} />
+            </div>
+          )}
+        </main>
+      )}
     </>
   );
 }
